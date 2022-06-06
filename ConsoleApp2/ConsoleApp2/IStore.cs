dd50dbd361ef3c78bc874f9d3db3899d817b2d9e
Khagani89 <khaganichh@code.edu.az>
2022-06-06 11:51:48 +0400

    project

---
+++ ConsoleApp2/ConsoleApp2/IStore.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    public interface IStore
+    {
+        string Products { get; }
+
+        int ProductLimit { get; }
+
+        int TotalIncome { get; }
+
+      void AddProduct(Product[] product) 
+        {
+           
+        }
+        void SellProduct(string no) 
+        {
+        
+        
+        }
+        
+
+
+    
+    
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    internal class Product
+    {
+        public int No;
+        public string Name;
+        public int Price;
+        public int Count;
+
+      
+    
+    }
+}
